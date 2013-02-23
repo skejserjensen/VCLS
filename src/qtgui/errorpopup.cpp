994249f7a82ad6651fdc8f78630ad75c794461b0
Soeren Kejser Jensen <devel@kejserjensen.dk>
2013-02-23 21:43:11 +0100

    Made the popup when a non supported log file is selected a bit clearer

--- src/qtgui/errorpopup.cpp
+++ src/qtgui/errorpopup.cpp
@@ -5,7 +5,7 @@ ErrorPopup::ErrorPopup(QWidget *parent) : QMessageBox(parent)
 {
     addButton(QMessageBox::Ok);
     setWindowTitle("Unsupported log type");
-    setText("The log type is unsupported, press OK to continue.");
+    setText("The file opened is not a supported log file, please select a file containing a log from a supported version control system.");
 }
 
 /** Destructor **/
